6562b7649040601b43b64dab8ef307316df44e72
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2024-09-23 13:07:33 +0530

    refactor: remove extra files

--- src/components/CallCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CallCard = () => {
-  return (
-    <div>CallCard</div>
-  )
-}
-
-export default CallCard
--- src/pages/JoinCall.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const JoinCall = () => {
-  return (
-    <div>JoinCall</div>
-  )
-}
-
-export default JoinCall
